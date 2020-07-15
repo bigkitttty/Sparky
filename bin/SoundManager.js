0d634368dd198f6adfa51452cba542764588e904
bigkitttty <caleb.n.coblentz@gmail.com>
2020-07-15 11:28:59 -0700

    Revert "soundm"
    This reverts commit 2cea74a673de4067b1afd033f50dff84ece45a37.

--- bin/SoundManager.js
+++ bin/SoundManager.js
@@ -1,24 +1,24 @@
 
-function _SoundManagerAdd-sl(name, filename) {
+function _SoundManagerAdd(name, filename) {
 	Module.SoundManagerAdd(Pointer_stringify(name), Pointer_stringify(filename));
 }
 
-function _SoundManagerPlay-sk(name) {
+function _SoundManagerPlay(name) {
 	Module.SoundManagerPlay(Pointer_stringify(name));
 }
 
-function _SoundManagerPause-sk(name) {
+function _SoundManagerPause(name) {
 	Module.SoundManagerPause(Pointer_stringify(name));
 }
 
-function _SoundManagerStop-sk(name) {
+function _SoundManagerStop(name) {
 	Module.SoundManagerStop(Pointer_stringify(name));
 }
 
-function _SoundManagerLoop-sk(name) {
+function _SoundManagerLoop(name) {
 	Module.SoundManagerLoop(Pointer_stringify(name));
 }
 
-function _SoundManagerSetGain-sk(name, gain) {
+function _SoundManagerSetGain(name, gain) {
 	Module.SoundManagerSetGain(Pointer_stringify(name), gain);
 }
